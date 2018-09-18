ad6c599b59d70ac4e1fd8818bf00ca5b8e923989
Some-dude-a-long-time-ago <43387168+Some-dude-a-long-time-ago@users.noreply.github.com>
2018-09-18 21:27:48 +0200

    cargo check

---
+++ HelloWorld/hello_cargo/target/debug/deps/hello_cargo-12d0cd4545e36438.d
@@ -0,0 +1,5 @@
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\HelloWorld\hello_cargo\target\debug\deps\hello_cargo-12d0cd4545e36438.rmeta: src\main.rs
+
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\HelloWorld\hello_cargo\target\debug\deps\hello_cargo-12d0cd4545e36438.d: src\main.rs
+
+src\main.rs:
---
+++ HelloWorld/hello_cargo/target/debug/libhello_cargo.d
@@ -0,0 +1,1 @@
+E:\Projects\Ghit-Hub\Rust\ Tutorials\Learning-Rust-Projects\HelloWorld\hello_cargo\target\debug\libhello_cargo.rmeta: E:\Projects\Ghit-Hub\Rust\ Tutorials\Learning-Rust-Projects\HelloWorld\hello_cargo\src\main.rs
