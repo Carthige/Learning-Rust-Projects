0749e936b82733447da1e05db1dbaf3b03834062
Some-dude-a-long-time-ago <43387168+Some-dude-a-long-time-ago@users.noreply.github.com>
2018-09-19 21:48:58 +0200

    added a rng

---
+++ Guessing Game/guessing_game/target/debug/deps/guessing_game-51c6406f016cc197.d
@@ -0,0 +1,5 @@
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Guessing Game\guessing_game\target\debug\deps\guessing_game-51c6406f016cc197.rmeta: src\main.rs
+
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Guessing Game\guessing_game\target\debug\deps\guessing_game-51c6406f016cc197.d: src\main.rs
+
+src\main.rs:
