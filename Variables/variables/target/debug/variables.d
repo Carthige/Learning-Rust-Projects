3fa3f519b08d7896e2f0c76c9bca475ce42968c8
Some-dude-a-long-time-ago <43387168+Some-dude-a-long-time-ago@users.noreply.github.com>
2018-09-27 22:19:48 +0200

    making things mutable

---
+++ Variables/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+E:\Projects\Ghit-Hub\Rust\ Tutorials\Learning-Rust-Projects\Variables\variables\target\debug\variables.pdb: E:\Projects\Ghit-Hub\Rust\ Tutorials\Learning-Rust-Projects\Variables\variables\src\main.rs
