ee4792230f6263316b0390661e0c4f4e7f2f4d01
Some-dude-a-long-time-ago <43387168+Some-dude-a-long-time-ago@users.noreply.github.com>
2018-10-23 22:16:31 +0200

    finisched scanner

--- H8 & H9 Threads and Locks/ip_sniffer/ip_sniffer/src/main.rs
+++ H8 & H9 Threads and Locks/ip_sniffer/ip_sniffer/src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
-use std::net::IpAddr;
+use std::io::{self, Write};
+use std::net::{IpAddr, TcpStream};
 use std::str::FromStr;
+use std::process;
+use std::sync::mpsc::{Sender, channel};
+use std::thread;
+
+const MAX: u16 = 65535;
 
 struct Arguments {
     flag: String,
@@ -32,19 +38,72 @@ impl Arguments {
                     Ok(s) => s,
                     Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6")
                 };
-                let threads = mantch args[2].parse::<u16>(){
+                let threads = match args[2].parse::<u16>(){
                     Ok(s) => s,
                     Err(_) => return Err("failed to parse thread number")
                 };
                 return Ok(Arguments{threads, flag, ipaddr});
+            } else {
+                return Err("Invalid Syntax");
             }
         }
     }
 }
 
+fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+    let mut port: u16 = start_port + 1;
+    loop {
+        match TcpStream::connect((addr, port)) {
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) =>  {}
+        }
+        
+        if (MAX -port) <= num_threads {
+            break;
+        }
+        port += num_threads;
+    }
+}
+
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
+    let arguments = Arguments::new(&args).unwrap_or_else(
+        |err| {
+            if err.contains("help") {  
+                process::exit(0);
+            } else {
+                eprintln!("{} problem parsing arguments: {}", program, err);
+                process::exit(0);
+            }
+        }
+    );
 
-    let flag
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx) = channel();
+    for i in 0.. num_threads{
+        let tx = tx.clone();        
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v );
+    }
 }
