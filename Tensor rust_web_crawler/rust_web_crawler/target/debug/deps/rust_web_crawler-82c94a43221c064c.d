c9523c8fa7cbf668e001f5b624c043b36407740e
Some-dude-a-long-time-ago <43387168+Some-dude-a-long-time-ago@users.noreply.github.com>
2018-11-11 14:19:30 +0100

    web crawler part 2

--- Tensor rust_web_crawler/rust_web_crawler/target/debug/deps/rust_web_crawler-82c94a43221c064c.d
+++ Tensor rust_web_crawler/rust_web_crawler/target/debug/deps/rust_web_crawler-82c94a43221c064c.d
@@ -1,5 +1,8 @@
-E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Tensor rust_web_crawler\rust_web_crawler\target\debug\deps\rust_web_crawler-82c94a43221c064c.exe: src\main.rs
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Tensor rust_web_crawler\rust_web_crawler\target\debug\deps\rust_web_crawler-82c94a43221c064c.exe: src\main.rs src\parse.rs src\fetch.rs src\crawler.rs
 
-E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Tensor rust_web_crawler\rust_web_crawler\target\debug\deps\rust_web_crawler-82c94a43221c064c.d: src\main.rs
+E:\Projects\Ghit-Hub\Rust Tutorials\Learning-Rust-Projects\Tensor rust_web_crawler\rust_web_crawler\target\debug\deps\rust_web_crawler-82c94a43221c064c.d: src\main.rs src\parse.rs src\fetch.rs src\crawler.rs
 
 src\main.rs:
+src\parse.rs:
+src\fetch.rs:
+src\crawler.rs:
